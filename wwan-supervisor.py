b9eae828948f4899688085346c36b457deabd821
Lapinot <peio.borthelle@ens-lyon.fr>
2020-07-09 19:32:45 +0200

    syslog logger, timeout handler

--- wwan-supervisor.py
+++ wwan-supervisor.py
@@ -6,48 +6,49 @@ import json, shlex, subprocess, time, traceback, sys
 # reference: rooter/package/rooter/0drivers/rqmi/files/usr/lib/rooter/qmi/connectqmi.sh
 
 
-UQMI = '/sbin/uqmi -s -d /dev/cdc-wdm0'
+UQMI = ['/sbin/uqmi', '-s', '-d', '/dev/cdc-wdm0']
 IFACE = 'wwan0'
 APN =  'free'
 
 def log(msg):
-    sys.stdout.write('{} {}\n'.format(datetime.now(), msg))
+    run_cmd('/bin/logger', '-s', '-t', 'wwan-supervisor', shlex.quote(msg))
 
-def run_cmd(cmd):
+def run_cmd(*cmd, capture=True, timeout=5, **kw):
     #log(f'DEBUG: CMD={cmd}')
-    proc = subprocess.run(shlex.split(cmd), capture_output=True, text=True)
-    return proc.stdout
+    proc = subprocess.run(cmd, capture_output=capture, text=True, timeout=timeout, **kw)
+    return proc
     
 
 def fix_network():
-    log('INFO: Stopping network...')
-    run_cmd(f'{UQMI} --stop-network 0xffffffff --autoconnect')
+    log('stopping network')
+    run_cmd(*UQMI, '--stop-network', '0xffffffff', '--autoconnect')
 
-    log(f'INFO: Starting network, APN={APN}')
-    cid = run_cmd(f'{UQMI} --get-client-id wds')
-    run_cmd(f'{UQMI} --set-client-id wds,{cid} --start-network {APN} --auth-type none --autoconnect')
-    log(f'INFO: Done')
+    log(f'restarting network (apn: {APN})')
+    cid = run_cmd(*UQMI, '--get-client-id', 'wds').stdout
+    run_cmd(*UQMI, '--set-client-id', f'wds,{cid}', '--start-network', APN, '--auth-type', 'none', '--autoconnect')
+    run_cmd(*UQMI, '--set-client-id', f'wds,{cid}', '--release-client-id')
+    log('done')
 
     return
 
     # TODO: do ipconf?
-    conf = json.loads(run_cmd(f'{UQMI} --set-client-id wds,{cid} --get-current-settings'))
+    #conf = json.loads(run_cmd(f'{UQMI} --set-client-id wds,{cid} --get-current-settings'))
 
-    gateway = conf['ipv4']['gateway']
-    address = conf['ipv4']['ip']
-    addr_o = ip_interface('{}/{}'.format(conf['ipv4']['ip'], conf['ipv4']['subnet']))
-    network = addr_o.network
-    address = str(addr_o)
+    #gateway = conf['ipv4']['gateway']
+    #address = conf['ipv4']['ip']
+    #addr_o = ip_interface('{}/{}'.format(conf['ipv4']['ip'], conf['ipv4']['subnet']))
+    #network = addr_o.network
+    #address = str(addr_o)
 
 
-    log(f'INFO: IP={address}, GATEWAY={gateway}')
+    #log(f'INFO: IP={address}, GATEWAY={gateway}')
 
-    run_cmd(f'/sbin/ip address flush dev {IFACE}')
-    run_cmd(f'/sbin/ip route flush dev {IFACE}')
+    #run_cmd(f'/sbin/ip address flush dev {IFACE}')
+    #run_cmd(f'/sbin/ip route flush dev {IFACE}')
 
-    run_cmd(f'/sbin/ip address add {address} dev {IFACE}')
-    run_cmd(f'/sbin/ip route add {network} dev {IFACE}')
-    run_cmd(f'/sbin/ip route add default via {gateway} dev {IFACE}')
+    #run_cmd(f'/sbin/ip address add {address} dev {IFACE}')
+    #run_cmd(f'/sbin/ip route add {network} dev {IFACE}')
+    #run_cmd(f'/sbin/ip route add default via {gateway} dev {IFACE}')
 
 
 
@@ -55,11 +56,13 @@ while True:
     try:
         status = json.loads(run_cmd(f'{UQMI} --get-data-status'))
         if status != 'connected':
-            log(f'INFO: DATA_STATUS={status}')
+            log(f'bad status: {status}')
             fix_network()
-        sys.stdout.flush()
-        time.sleep(3)
+    except subprocess.TimeoutExpired:
+        log('uqmi timeout, killing everything')
+        run_cmd('/usr/bin/killall', '-w', '/sbin/uqmi', timeout=None)
     except Exception:
-        log('ERROR: <<<')
-        traceback.print_exc(file=sys.stdout)
-        log('ERROR: >>>')
+        for line in traceback.format_exc().splitlines():
+            log(f'error: {line}')
+    finally:
+        time.sleep(3)
